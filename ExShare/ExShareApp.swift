281b49813b9fc257d2ce315c360ad3eec2793b43
jake <palatable7@naver.com>
2022-09-07 23:39:21 +0900

    Initial Commit

---
+++ ExShare/ExShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExShareApp.swift
+//  ExShare
+//
+//  Created by 김종권 on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct ExShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
