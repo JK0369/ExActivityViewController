7f4fe567312ad987dc880987b47bb9fbbfeb5294
jake <palatable7@naver.com>
2022-09-08 00:13:01 +0900

    Implement

--- ExShare/ContentView.swift
+++ ExShare/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var isSharePresented = false
+  let image = UIImage(systemName: "star")
+  
+  var body: some View {
+    Button("공유하기") {
+      self.isSharePresented = true
     }
+    .sheet(
+      isPresented: $isSharePresented,
+      onDismiss: { print("Dismiss") },
+      content: { ActivityViewController(activityItems: [image]) }
+    )
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct ActivityViewController: UIViewControllerRepresentable {
+  var activityItems: [Any]
+  var applicationActivities: [UIActivity]? = nil
+  @Environment(\.presentationMode) var presentationMode
+  
+  func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>
+  ) -> UIActivityViewController {
+    let controller = UIActivityViewController(
+      activityItems: activityItems,
+      applicationActivities: applicationActivities
+    )
+    controller.completionWithItemsHandler = { (activityType, completed, returnedItems, error) in
+      self.presentationMode.wrappedValue.dismiss()
     }
+    return controller
+  }
+  
+  func updateUIViewController(
+    _ uiViewController: UIActivityViewController,
+    context: UIViewControllerRepresentableContext<ActivityViewController>
+  ) {}
 }
